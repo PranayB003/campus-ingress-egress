e6aa738c33bd6fc0db2d0fa8d851f523ec56898f
PranayB003 <pranayborgohain@gmail.com>
2023-01-22 22:38:27 +0530

    connected: database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { StudentsModule } from './students/students.module';
+import { TripsModule } from './trips/trips.module';
+
+dotenv.config();
+
+const { MONGO_HOST, MONGO_PORT, MONGO_USERNAME, MONGO_PASSWORD, ENVIRONMENT } =
+  process.env;
+const dbName = ENVIRONMENT === 'dev' ? 'dev' : 'prod';
+const connectionString = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@campus-in-out-db@&authSource=${dbName}`;
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(connectionString),
+    StudentsModule,
+    TripsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
